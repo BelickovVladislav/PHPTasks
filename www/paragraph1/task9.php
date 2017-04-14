e9509ff0a807a289360c5e1159b99ab6a99e6198
BelickovVladislav <belickov.vlad1997@yandex.ru>
2017-04-14 04:03:53 +0300

    Performed Task

--- www/paragraph1/task9.php
+++ www/paragraph1/task9.php
@@ -4,7 +4,7 @@
  * Автоморфное число  равно последним разрядам квадрата этого числа.
  * Например,  5 и 25,  6 и 36,  25 и 625.
  */
-function getBitNimber($number)
+function getBitNumber($number)
 {
     $bit = 1;
     while ($number != 0) {
@@ -13,9 +13,12 @@ function getBitNimber($number)
     }
     return $bit;
 }
-function isAutomorphicNumber($number){
-    return ($number*$number)%getBitNimber($number) == $number;
+
+function isAutomorphicNumber($number)
+{
+    return ($number * $number) % getBitNumber($number) == $number;
 }
+
 $number = 10;
-echo "Число $number ".(isAutomorphicNumber($number)?"":"не ")."является автоморфным.";
+echo "Число $number " . (isAutomorphicNumber($number) ? "" : "не ") . "является автоморфным.";
 ?>
