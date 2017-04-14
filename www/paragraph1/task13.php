1db6047a8f494ac7e217e48991f5536cced0e139
BelickovVladislav <belickov.vlad1997@yandex.ru>
2017-04-15 02:45:07 +0300

    Performed Task

---
+++ www/paragraph1/task13.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * Определить, является ли число  2^n + m  симметричным,
+ * т. е. запись  числа содержит четное количество цифр и совпадают его левая и правая  половинки.
+ */
+function getCountBitNumber($number)
+{
+    $count = 0;
+    while ($number != 0) {
+        $count++;
+        $number = (int)($number / 10);
+    }
+    return $count;
+}
+
+function reverse($number)
+{
+    $m = 0;
+    while ($number != 0) {
+        $m *= 10;
+        $m += $number % 10;
+        $number = (int)($number / 10);
+    }
+    return $m;
+}
+
+function isSymmetric($number)
+{
+    $countBit = getCountBitNumber($number);
+    if ($countBit % 2 == 0) {
+        $countBit = (int)($countBit / 2);
+        $count = 0;
+        $number2 = 0;
+        while ($count < $countBit) {
+            $number2 *= 10;
+            $number2 += $number % 10;
+            $number = (int)($number / 10);
+            $count++;
+        }
+        while (getCountBitNumber($number) > getCountBitNumber($number2))
+            $number = (int)($number / 10);
+        return $number == reverse($number2);
+    }
+    return false;
+}
+
+$n = 12;
+$m = -56;
+$number = 1;
+for ($i = 0; $i < $n; $i++) {
+    $number *= 2;
+}
+$number += $m;
+echo "Число $number " . (isSymmetric($number) ? "" : "не ") . "является симметричным.";
+
+?>
