78f5034dc7a66c0cdec72ea7b442d76b5945d396
BelickovVladislav <belickov.vlad1997@yandex.ru>
2017-04-19 12:09:57 +0300

    Performed Task

--- parserXML/index.php
+++ parserXML/index.php
@@ -3,11 +3,13 @@ header("Content-Type: text/html; charset=utf-8");
 /*
  *  Спарсить 10 последних новостей с  rss https://realt.onliner.by/
  */
-function deleteSpecialSymbols($str){
-    $str = str_replace("Читать далее…",'',strip_tags($str));
-       $str = preg_replace("[&nbsp;]"," ",$str);
+function deleteSpecialSymbols($str)
+{
+    $str = str_replace("Читать далее…", '', strip_tags($str));
+    $str = preg_replace("[&nbsp;]", " ", $str);
     return $str;
 }
+
 $xml = simplexml_load_file('https://realt.onliner.by/feed');
 //$xml = new SimpleXMLIterator($xml->__toString());
 $addFile = array();
@@ -18,7 +20,7 @@ for ($i = 0; $i < 10; $i++) {
     $child = $item->children("media", true);
     $thumbnail = $child[0]->attributes();
 //    echo '<pre>' .print_r($child, true). '</pre>';
-    $addFile[$i]['title'] =deleteSpecialSymbols($item->title);
+    $addFile[$i]['title'] = deleteSpecialSymbols($item->title);
     $addFile[$i]['picture'] = deleteSpecialSymbols($thumbnail['url']);
     $addFile[$i]['link'] = deleteSpecialSymbols($item->link);
     $addFile[$i]['description'] = deleteSpecialSymbols($item->description);
